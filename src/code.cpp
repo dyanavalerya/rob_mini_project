a4d816e07667253afbf7563c8ec333b702585a29
Rasmus N. Skjelsager <rskjel18@student.aau.dk>
2018-12-27 17:21:27 +0100

    Update code.cpp
    Restructure and added prototype functions

--- src/code.cpp
+++ src/code.cpp
@@ -10,6 +10,34 @@ std::string choice2 = "choice2";
 int player1points = 0, player2points = 0;
 bool stop = false;
 
+void gameCallback1(const std_msgs::String::ConstPtr& msg);
+void gameCallback2(const std_msgs::String::ConstPtr& msg);
+void compare(ros::Publisher publisher);
+
+
+int main(int argc, char **argv){
+
+  ros::init(argc, argv, "core");
+  ros::NodeHandle nh;
+  ros::Subscriber sub1 = nh.subscribe("choice1", 20, gameCallback1);
+  ros::Subscriber sub2 = nh.subscribe("choice2", 20, gameCallback2);
+  ros::Publisher finish = nh.advertise<std_msgs::Bool>("/finish", 10);
+
+  ros::Duration(1.0).sleep();
+
+  while(ros::ok() && !stop){
+  ros::spinOnce();
+    if(choice1 != "choice1" && choice2 != "choice2"){
+      compare(finish);
+      choice1 = "choice1";
+      choice2 = "choice2";
+    }
+  }
+
+
+  return 0;
+}
+
 void gameCallback1(const std_msgs::String::ConstPtr& msg)
 {
   choice1 = msg->data;
@@ -20,7 +48,7 @@ void gameCallback2(const std_msgs::String::ConstPtr& msg)
   choice2 = msg->data;
 }
 
-void compare(ros::Publisher publisher){
+void compare(){
 
     if(choice1 == "rock" && choice2 == "scissor") {
       ROS_INFO("1p goes to Player 1!");
@@ -67,27 +95,4 @@ void compare(ros::Publisher publisher){
       stop2.data = stop;
       publisher.publish(stop2);
     }
-}
-
-int main(int argc, char **argv){
-
-  ros::init(argc, argv, "core");
-  ros::NodeHandle nh;
-  ros::Subscriber sub1 = nh.subscribe("choice1", 20, gameCallback1);
-  ros::Subscriber sub2 = nh.subscribe("choice2", 20, gameCallback2);
-  ros::Publisher finish = nh.advertise<std_msgs::Bool>("/finish", 10);
-
-  ros::Duration(1.0).sleep();
-
-  while(ros::ok() && !stop){
-  ros::spinOnce();
-    if(choice1 != "choice1" && choice2 != "choice2"){
-      compare(finish);
-      choice1 = "choice1";
-      choice2 = "choice2";
-    }
-  }
-
-
-  return 0;
 }

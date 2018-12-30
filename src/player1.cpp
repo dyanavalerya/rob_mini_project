ae5aaa8b30751a4b50283b67c62fe221dcf61174
dyanavalerya <32998102+dyanavalerya@users.noreply.github.com>
2018-12-30 16:51:36 +0100

    Update player1.cpp

--- src/player1.cpp
+++ src/player1.cpp
@@ -6,7 +6,6 @@
 bool stop = false;
 void finishCallBack(const std_msgs::Bool::ConstPtr& msg)
 {
-  ROS_INFO("l-a luat");
   stop = msg->data;
 }
 
@@ -22,12 +21,12 @@ int main(int argc, char** argv){
   ros::Subscriber sub = nh.subscribe("/finish", 10, finishCallBack);
 
   while(ros::ok() && !stop){
-    std::cout<<"Enter the player 1 choice: (rock/ paper/ scisor) ";
+    ROS_INFO("Enter the player 1 choice: (rock/ paper/ scisor)");
     std::cin>>choice1.data;
-    std::cout<<"\n";
 
     if(choice1.data == "rock" || choice1.data == "paper" || choice1.data == "scissor"){pub.publish(choice1);}
     loop_rate.sleep();
+    ros::spinOnce();
   }
 
   return 0;
